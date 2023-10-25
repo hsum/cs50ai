723ba051ce591de4265640841769d50e49ae95a9
Martin J. Hsu <martin.hsu@gmail.com>
2023-10-25 22:19:50 +0800

    implemented actions(), result(), winner(), terminal(), and utility()

--- 0/tictactoe/tictactoe.py
+++ 0/tictactoe/tictactoe.py
@@ -9,6 +9,13 @@ O = "O"
 EMPTY = None
 
 
+def print_(board):
+    print('\n'.join(
+        ' | '.join(f'{i=} {j=} {column}' for j, column in enumerate(row))
+        for i, row in enumerate(board)
+    ))
+
+
 def initial_state():
     """
     Returns starting state of the board.
@@ -22,11 +29,7 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    count = 0
-    for row in board:
-        for column in row:
-            if column != EMPTY:
-                count += 1
+    count = sum(1 for row in board for column in row if column != EMPTY)
     return (X, O)[count % 2]
 
 
@@ -34,35 +37,61 @@ def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    return set((i, j) for i, row in enumerate(board) for j, column in enumerate(row) if column == EMPTY)
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
-    raise NotImplementedError
+    i, j = action
+    new_board = board[:]
+    new_board[i][j] = player(board)
+    return new_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return O if player(board) == X else X
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    win_condition_ = (
+        ((0, 0), (0, 1), (0, 2)),
+        ((1, 0), (1, 1), (1, 2)),
+        ((2, 0), (2, 1), (2, 2)),
+
+        ((0, 0), (1, 1), (2, 2)),
+        ((2, 0), (1, 1), (0, 2)),
+
+        ((0, 0), (1, 0), (2, 0)),
+        ((1, 0), (1, 1), (1, 2)),
+        ((2, 0), (2, 1), (2, 2)),
+
+    )
+    for wc in win_condition_:
+        candidate = tuple(board[i][j] for i, j in wc if board[i][j] != EMPTY)
+        if len(candidate) == 3 and len(set(candidate)) == 1:
+            return True
+    return False
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    return {
+        X: 1,
+        O: -1
+    }.get(winner(board), 0)
+
 
 
 def minimax(board):
